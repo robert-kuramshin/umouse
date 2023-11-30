6854c1e90a79411142660489d79a84458e4d1d93
Robert <robert.kuramshin98@gmail.com>
2023-11-29 19:15:40 -0800

    encoders

--- example/hello_usb.c
+++ example/hello_usb.c
@@ -18,22 +18,15 @@
 #include "VL53L1X_types.h"
 #include <math.h>
 
+#include "encoders.h"
+
 #define RIGHT_MOTOR_A_PIN (14)
 #define RIGHT_MOTOR_B_PIN (15)
-#define RIGHT_ENCODER_A_PIN (12)
-#define RIGHT_ENCODER_B_PIN (13)
 
 #define LEFT_MOTOR_A_PIN (1)
 #define LEFT_MOTOR_B_PIN (0)
-#define LEFT_ENCODER_A_PIN (2)
-#define LEFT_ENCODER_B_PIN (3)
 
 #define WRAP (50000)
-
-#define COUNTS_PER_REV (12)
-#define SECONDS_PER_MIN (60)
-#define MEASURE_PERIOD_SEC (3)
-#define SEC_TO_uS (1000000)
 
 #define I2C_DEV_ADDR 0x29
 
@@ -71,25 +64,6 @@ uint lrchan;
 //     printf("event A\n");
 // }
 
-void gpio_callback_b(uint gpio, uint32_t events)
-{
-  // printf("event B\n");
-  if (events & GPIO_IRQ_EDGE_FALL)
-  {
-    counter += 1;
-  }
-}
-
-bool timer_callback(repeating_timer_t *rt)
-{
-  uint32_t count = counter;
-  counter = 0;
-
-  uint32_t rpm = (count * (SECONDS_PER_MIN / MEASURE_PERIOD_SEC)) / COUNTS_PER_REV;
-
-  printf("RPM: %d COUNT: %d \n", rpm, count);
-  return true; // keep repeating
-}
 
 VL53L1X_Status_t tof_init()
 {
@@ -407,6 +381,8 @@ int main()
   gpio_pull_up(LEFT_ENCODER_A_PIN);
   gpio_pull_up(LEFT_ENCODER_B_PIN);
 
+  encoders_register_callbacks();
+  
   // gpio_set_irq_enabled_with_callback(RIGHT_ENCODER_B_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_callback_b);
   // // basic_sequence_test();
   // printf("Freq %d\n", 125000000/WRAP);
---
+++ example/include/encoders.h
@@ -0,0 +1,29 @@
+#ifndef ENCODERS_H
+#define ENCODERS_H
+
+#define COUNTS_PER_REV (15.201)
+#define SECONDS_PER_MIN (60)
+#define MEASURE_PERIOD_SEC (1)
+#define SEC_TO_uS (1000000)
+
+#define WHEEL_DIAMETER_MM (32.5)
+#define WHEEL_CIRCUMFERENCE_MM (106.81)
+
+#define CALIB (1)
+
+#define RIGHT_ENCODER_A_PIN (12)
+#define RIGHT_ENCODER_B_PIN (13)
+
+#define LEFT_ENCODER_A_PIN (2)
+#define LEFT_ENCODER_B_PIN (3)
+
+void encoders_register_callbacks(void);
+
+void encoders_zero_distances(void);
+
+float encoders_distance_traveled(void);
+
+bool timer_callback(repeating_timer_t *rt);
+
+#endif // ENCODERS_H
+
