6379a6d7a2508f1aa2194bced2416c533b705bf3
Robert <robert.kuramshin98@gmail.com>
2024-01-20 17:44:46 -0800

    3x tof yuhyeet

--- example/hello_usb.c
+++ example/hello_usb.c
@@ -29,7 +29,9 @@
 
 #define WRAP (50000)
 
-#define I2C_DEV_ADDR 0x29
+#define I2C_DEV_ADDR_0 0x29
+#define I2C_DEV_ADDR_1 0x39
+#define I2C_DEV_ADDR_2 0x49
 
 #define RIGHT_ADC_PIN (26)
 #define RIGHT_ADC_INDEX (0)
@@ -50,7 +52,7 @@ int ir_dist_index = 0;
 
 volatile uint32_t counter = 0;
 
-volatile uint16_t tof_distance = 50;
+volatile uint16_t tof_distance[3] = {50,50,50};
 
 bool moving = true;
 // motor slices and f/r channels
@@ -66,30 +68,45 @@ uint lrchan;
 // }
 
 
-VL53L1X_Status_t tof_init()
+VL53L1X_Status_t tof_init(int xshut, uint16_t addr)
 {
   VL53L1X_Status_t status;
   // GPIO sda=16, scl=17, i2c0
-  if (VL53L1X_I2C_Init(I2C_DEV_ADDR, i2c0) < 0)
+  // gpio_pull_up(xshut);
+  gpio_put(xshut,1);
+
+  if (VL53L1X_I2C_Init(I2C_DEV_ADDR_0, i2c0) < 0)
   {
     printf("Error initializing sensor.\n");
     return 0;
   }
   printf("we are done\n");
+  // change address
+  if (addr != I2C_DEV_ADDR_0)
+  {
+    int ret = VL53L1X_SetI2CAddress(I2C_DEV_ADDR_0, addr<<1);
+    if (ret != 0)
+    {
+      printf("Could not set sensor addr to %d\n", addr);
+    } else {
+      printf("address changed\n");
+    }
+  }
   // Ensure the sensor has booted
   uint8_t sensorState;
   do
   {
-    status += VL53L1X_BootState(I2C_DEV_ADDR, &sensorState);
-    VL53L1X_WaitMs(I2C_DEV_ADDR, 2);
+    status += VL53L1X_BootState(addr, &sensorState);
+    VL53L1X_WaitMs(addr, 2);
   } while (sensorState == 0);
-  printf("Sensor booted.\n");
+  printf("Sensor booted. 0x%x\n",addr);
+
   // Initialize and configure sensor
-  status = VL53L1X_SensorInit(I2C_DEV_ADDR);
-  status += VL53L1X_SetDistanceMode(I2C_DEV_ADDR, 1);
-  status += VL53L1X_SetTimingBudgetInMs(I2C_DEV_ADDR, 100);
-  status += VL53L1X_SetInterMeasurementInMs(I2C_DEV_ADDR, 100);
-  status += VL53L1X_StartRanging(I2C_DEV_ADDR);
+  status = VL53L1X_SensorInit(addr);
+  status += VL53L1X_SetDistanceMode(addr, 1);
+  status += VL53L1X_SetTimingBudgetInMs(addr, 100);
+  status += VL53L1X_SetInterMeasurementInMs(addr, 100);
+  status += VL53L1X_StartRanging(addr);
 
   return status;
 }
@@ -337,35 +354,59 @@ void counter_total_zero_test()
 void core1_entry()
 {
   // init tof sensor
-  VL53L1X_Result_t results;
-  VL53L1X_Status_t status = tof_init();
+  VL53L1X_Result_t results[3] = {0};
+  VL53L1X_Status_t status[3] = {0};
+  i2c_init(i2c0, VL53L1X_I2C_BAUDRATE);
+  gpio_set_function(16, GPIO_FUNC_I2C);
+  gpio_set_function(17, GPIO_FUNC_I2C);
+  gpio_pull_up(16);
+  gpio_pull_up(17);
+  gpio_init(22);
+  gpio_set_dir(22,GPIO_OUT);
+  gpio_put(22,0);
+  gpio_init(18);
+  gpio_set_dir(18,GPIO_OUT);
+  gpio_put(18,0);
+  gpio_init(28);
+  gpio_set_dir(28,GPIO_OUT);
+  gpio_put(28,0);
+  uint16_t addrs[3] ={I2C_DEV_ADDR_2, I2C_DEV_ADDR_1, I2C_DEV_ADDR_0};
+  status[0] = tof_init(22,I2C_DEV_ADDR_2);
+  printf("TOF 0 status %d\n",status[0]);
+  status[1] = tof_init(18,I2C_DEV_ADDR_1);
+  printf("TOF 1 status %d\n",status[1]);
+  status[2] = tof_init(28,I2C_DEV_ADDR_0);
+  printf("TOF 2 status %d\n",status[2]);
   printf("Done configuring sensor!\n");
-  bool first_range = true;
+  bool first_range[3] = {true,true,true};
   while (true)
   {
-    // Wait until we have new data (front distance)
-    uint8_t dataReady;
-    do
+    for(int i = 0; i < 3;i++)
     {
-      status = VL53L1X_CheckForDataReady(I2C_DEV_ADDR, &dataReady);
-      sleep_us(1);
-    } while (dataReady == 0);
+      // Wait until we have new data (front distance)
+      uint8_t dataReady;
+      do
+      {
+        status[i] = VL53L1X_CheckForDataReady(addrs[i], &dataReady);
+        sleep_us(1);
+      } while (dataReady == 0);
 
-    // Read and display result
-    status += VL53L1X_GetResult(I2C_DEV_ADDR, &results);
+      // Read and display result
+      status[i] += VL53L1X_GetResult(addrs[i], &results[i]);
 
-    if (results.distance > 10)
-    {
-      tof_distance = results.distance;
-    }
-    // printf("Core 1 TOF distance: %5d\n", tof_distance);
+      if (results[i].distance > 10)
+      {
+        tof_distance[i] = results[i].distance;
+      }
+      printf("Core 1 TOF %d distance: %5d\n",i,  tof_distance[i]);
 
-    status += VL53L1X_ClearInterrupt(I2C_DEV_ADDR);
-    if (first_range)
-    { // Clear twice on first measurement
-      status += VL53L1X_ClearInterrupt(I2C_DEV_ADDR);
-      first_range = false;
-    }
+      status[i] += VL53L1X_ClearInterrupt(addrs[i]);
+      if (first_range[i])
+      { // Clear twice on first measurement
+        status[i] += VL53L1X_ClearInterrupt(addrs[i]);
+        first_range[i] = false;
+      }
+      }
   }
 }
 
@@ -383,136 +424,140 @@ int main()
   sleep_ms(1000);
   cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
   sleep_ms(1000);
-  printf("Board initialized!");
+  printf("Board initialized!\n");
 
   // Initialize adc pins
   adc_gpio_init(RIGHT_ADC_PIN);
   adc_gpio_init(LEFT_ADC_PIN);
 
   multicore_launch_core1(core1_entry);
-
-  // // Init motor output right and left (both forward)
-  initMotors();
-  gpio_pull_up(RIGHT_ENCODER_A_PIN);
-  gpio_pull_up(RIGHT_ENCODER_B_PIN);
-  gpio_pull_up(LEFT_ENCODER_A_PIN);
-  gpio_pull_up(LEFT_ENCODER_B_PIN);
-
-  encoders_register_callbacks();
-  
-  // gpio_set_irq_enabled_with_callback(RIGHT_ENCODER_B_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_callback_b);
-  // // basic_sequence_test();
-  // printf("Freq %d\n", 125000000/WRAP);
-
-  // Init interrupts
-  // gpio_set_irq_enabled_with_callback(ENCODER_A_PIN, GPIO_IRQ_LEVEL_HIGH, true, &gpio_callback_a);
-
-  // Repeating timer
-  float prev_right_avg = 0.0f;
-  float prev_left_avg = 0.0f;
-  q[0] = 'R'; // start off going R
-  // q[1] = 'R';
-  //  q[2] = 'R';
-  //  q[3] = 'R';
-  //  q[4] = 'L';
-  q_index = 0;
-  while (true)
+  while(true)
   {
-    // if (ir_dist_index >= 5)
-    // {
-    //   for (int i = 0; i < 5; i++)
-    //   {
-    //     prev_right_avg += right_dists[i];
-    //     prev_left_avg += left_dists[i];
-    //   }
-    //   prev_right_avg /= 5;
-    //   prev_left_avg /= 5;
-    // }
-    // update ir distance arrays with new distances
-    right_dists[ir_dist_index % 5] = rightVoltage2Distance(adcData((RIGHT_ADC_INDEX)));
-    left_dists[ir_dist_index % 5] = leftVoltage2Distance(adcData(LEFT_ADC_INDEX));
-    ir_dist_index++;
-
-    // if (ir_dist_index > 5)
-    // {
-    //   float right_avg = 0.0f;
-    //   float left_avg = 0.0f;
-    //   for (int i = 0; i < 5; i++)
-    //   {
-    //     right_avg += right_dists[i];
-    //     left_avg += left_dists[i];
-    //   }
-    //   right_avg /= 5;
-    //   left_avg /= 5;
-    //   updateOdom();
-    //   if (right_avg - prev_right_avg > 0.1)
-    //   {
-    //     // add gap to right side of the map
-    //     // mouseUpdateWall();
-    //     mouseUpdateWall(-1,DRIGHT);
-    //   }
-    //   else
-    //   {
-    //     mouseUpdateWall(1,DRIGHT);
-    //     // add a right wall to map
-    //   }
-    //   if (left_avg - prev_left_avg > 0.1)
-    //   {
-    //     // add gap to left side in ma[p]
-    //     mouseUpdateWall(-1,DLEFT);
-    //   }
-    //   else
-    //   {
-    //     mouseUpdateWall(1,DLEFT);
-    //     // add a left wall to map
-    //   }
-    // }
-    // 80mm ~ pi inches
-    // printf("Core 2 TOF distance: %5d\n", tof_distance);
-    // printf(" dist = %5d, rightDist = %f, leftDist = %f\n", tof_distance, right_dists[(ir_dist_index - 1) % 5], left_dists[(ir_dist_index - 1) % 5]);
-    center_logic(tof_distance, right_dists[(ir_dist_index - 1) % 5], left_dists[(ir_dist_index - 1) % 5]);
-    if (!moving)
-    {
-      sleep_ms(1000);
-      // hardcoded directions for now to test the right motor, very unstable.
-      if (true)
-      {
-        // prompt left or right
-        printMaze();
-        printf("Go Left (L) or Right (R) ?\n");
-        char next_action = getchar_timeout_us(10 *1000*1000);
-        // char next_action = q[q_index];
-        if (next_action == 'R')
-        {
-          printf("Going right\n");
-          // this needs to use the encoder code (ie turn right for k cm)
-          turn_right(40);
-          sleep_ms(200);
-          brake(100);
-          mouseUpdateDir(DRIGHT);
-        }
-        else if (next_action == 'L')
-        {
-          printf("Going left\n");
-          // this needs to use the encoder code (ie turn right for k cm)
-          turn_left(50);
-          sleep_ms(300);
-          brake(100);
-          mouseUpdateDir(DLEFT);
-        }
-        else if (next_action == 'T')
-        {
-          printf("turning around\n");
-          // this needs to use the encoder code (ie turn right for k cm)
-          turn_right(30);
-          sleep_ms(600);
-          brake(100);
-          mouseUpdateDir(DRIGHT);
-          mouseUpdateDir(DRIGHT);
-        }
-      }
-      // zero encoders after turn
-      encoders_zero_distances();
-    }
+    ;
   }
+
+  // // // Init motor output right and left (both forward)
+  // initMotors();
+  // gpio_pull_up(RIGHT_ENCODER_A_PIN);
+  // gpio_pull_up(RIGHT_ENCODER_B_PIN);
+  // gpio_pull_up(LEFT_ENCODER_A_PIN);
+  // gpio_pull_up(LEFT_ENCODER_B_PIN);
+
+  // encoders_register_callbacks();
+  
+  // // gpio_set_irq_enabled_with_callback(RIGHT_ENCODER_B_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_callback_b);
+  // // // basic_sequence_test();
+  // // printf("Freq %d\n", 125000000/WRAP);
+
+  // // Init interrupts
+  // // gpio_set_irq_enabled_with_callback(ENCODER_A_PIN, GPIO_IRQ_LEVEL_HIGH, true, &gpio_callback_a);
+
+  // // Repeating timer
+  // float prev_right_avg = 0.0f;
+  // float prev_left_avg = 0.0f;
+  // q[0] = 'R'; // start off going R
+  // // q[1] = 'R';
+  // //  q[2] = 'R';
+  // //  q[3] = 'R';
+  // //  q[4] = 'L';
+  // q_index = 0;
+  // while (true)
+  // {
+  //   // if (ir_dist_index >= 5)
+  //   // {
+  //   //   for (int i = 0; i < 5; i++)
+  //   //   {
+  //   //     prev_right_avg += right_dists[i];
+  //   //     prev_left_avg += left_dists[i];
+  //   //   }
+  //   //   prev_right_avg /= 5;
+  //   //   prev_left_avg /= 5;
+  //   // }
+  //   // update ir distance arrays with new distances
+  //   right_dists[ir_dist_index % 5] = rightVoltage2Distance(adcData((RIGHT_ADC_INDEX)));
+  //   left_dists[ir_dist_index % 5] = leftVoltage2Distance(adcData(LEFT_ADC_INDEX));
+  //   ir_dist_index++;
+
+  //   // if (ir_dist_index > 5)
+  //   // {
+  //   //   float right_avg = 0.0f;
+  //   //   float left_avg = 0.0f;
+  //   //   for (int i = 0; i < 5; i++)
+  //   //   {
+  //   //     right_avg += right_dists[i];
+  //   //     left_avg += left_dists[i];
+  //   //   }
+  //   //   right_avg /= 5;
+  //   //   left_avg /= 5;
+  //   //   updateOdom();
+  //   //   if (right_avg - prev_right_avg > 0.1)
+  //   //   {
+  //   //     // add gap to right side of the map
+  //   //     // mouseUpdateWall();
+  //   //     mouseUpdateWall(-1,DRIGHT);
+  //   //   }
+  //   //   else
+  //   //   {
+  //   //     mouseUpdateWall(1,DRIGHT);
+  //   //     // add a right wall to map
+  //   //   }
+  //   //   if (left_avg - prev_left_avg > 0.1)
+  //   //   {
+  //   //     // add gap to left side in ma[p]
+  //   //     mouseUpdateWall(-1,DLEFT);
+  //   //   }
+  //   //   else
+  //   //   {
+  //   //     mouseUpdateWall(1,DLEFT);
+  //   //     // add a left wall to map
+  //   //   }
+  //   // }
+  //   // 80mm ~ pi inches
+  //   // printf("Core 2 TOF distance: %5d\n", tof_distance);
+  //   // printf(" dist = %5d, rightDist = %f, leftDist = %f\n", tof_distance, right_dists[(ir_dist_index - 1) % 5], left_dists[(ir_dist_index - 1) % 5]);
+  //   center_logic(tof_distance, right_dists[(ir_dist_index - 1) % 5], left_dists[(ir_dist_index - 1) % 5]);
+  //   if (!moving)
+  //   {
+  //     sleep_ms(1000);
+  //     // hardcoded directions for now to test the right motor, very unstable.
+  //     if (true)
+  //     {
+  //       // prompt left or right
+  //       printMaze();
+  //       printf("Go Left (L) or Right (R) ?\n");
+  //       char next_action = getchar_timeout_us(10 *1000*1000);
+  //       // char next_action = q[q_index];
+  //       if (next_action == 'R')
+  //       {
+  //         printf("Going right\n");
+  //         // this needs to use the encoder code (ie turn right for k cm)
+  //         turn_right(40);
+  //         sleep_ms(200);
+  //         brake(100);
+  //         mouseUpdateDir(DRIGHT);
+  //       }
+  //       else if (next_action == 'L')
+  //       {
+  //         printf("Going left\n");
+  //         // this needs to use the encoder code (ie turn right for k cm)
+  //         turn_left(50);
+  //         sleep_ms(300);
+  //         brake(100);
+  //         mouseUpdateDir(DLEFT);
+  //       }
+  //       else if (next_action == 'T')
+  //       {
+  //         printf("turning around\n");
+  //         // this needs to use the encoder code (ie turn right for k cm)
+  //         turn_right(30);
+  //         sleep_ms(600);
+  //         brake(100);
+  //         mouseUpdateDir(DRIGHT);
+  //         mouseUpdateDir(DRIGHT);
+  //       }
+  //     }
+  //     // zero encoders after turn
+  //     encoders_zero_distances();
+  //   }
+  // }
 }
